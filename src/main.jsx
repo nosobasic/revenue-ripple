949d224ec2627dc9c7ea0cadb71821d8cc533c46
nosobasic <wdonte97@gmail.com>
2025-05-12 10:41:17 -0400

    fix: remove duplicate AuthProvider to fix browser back button functionality

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
